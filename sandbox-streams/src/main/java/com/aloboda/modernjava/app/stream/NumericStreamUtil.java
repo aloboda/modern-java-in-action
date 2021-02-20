472e5946db8aab69535c36268df1626356bb20f4
Andrii Loboda <andrii.loboda@gmail.com>
2021-02-19 22:24:35 -0500

    Add pythagorean tuples calculation.

--- sandbox-streams/src/main/java/com/aloboda/modernjava/app/stream/NumericStreamUtil.java
+++ sandbox-streams/src/main/java/com/aloboda/modernjava/app/stream/NumericStreamUtil.java
@@ -1,10 +1,19 @@
 package com.aloboda.modernjava.app.stream;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class NumericStreamUtil {
     public static IntStream getNaturalEvenNumbers(final int lastNumberInclusive) {
         return IntStream.rangeClosed(1, lastNumberInclusive)
                 .filter(n -> n % 2 == 0);
+    }
+
+    public static Stream<int[]> getPythagoreanTriples(final int adjacentLastNumber) {
+        return IntStream.rangeClosed(1, adjacentLastNumber).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, adjacentLastNumber)
+                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                        .mapToObj(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)})
+                );
     }
 }
--- sandbox-streams/src/test/java/com/aloboda/modernjava/app/stream/NumericStreamUtilTest.java
+++ sandbox-streams/src/test/java/com/aloboda/modernjava/app/stream/NumericStreamUtilTest.java
@@ -3,11 +3,21 @@ package com.aloboda.modernjava.app.stream;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class NumericStreamUtilTest {
     @Test
     public void when_get_natural_even_numbers_then() {
         assertThat(NumericStreamUtil.getNaturalEvenNumbers(100).count(), Matchers.is(50L));
+    }
+
+    @Test
+    public void when_calculate_pythagorean_triangles_then_return_correct_tuples() {
+        final List<int[]> triangles = NumericStreamUtil.getPythagoreanTriples(100).skip(5).limit(5).collect(Collectors.toList());
+        assertThat(triangles.get(0), Matchers.equalTo(new int[]{9, 12, 15}));
+        assertThat(triangles.get(1), Matchers.equalTo(new int[]{9, 40, 41}));
     }
 }
